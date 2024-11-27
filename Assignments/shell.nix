c55d124fe0ea77e46be607d1ca406af5f790fa55
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-11-27 23:40:11 +0000

    Include cancel TeXlive package in shell.nix

--- Assignments/shell.nix
+++ Assignments/shell.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}: let
-  texlive = pkgs.texlive.withPackages (p: with p; [scheme-medium gensymb]);
+  texlive = pkgs.texlive.withPackages (p: with p; [scheme-medium gensymb cancel]);
 in
   pkgs.mkShell {
     buildInputs = [texlive] ++ (with pkgs; [just fd python3]);
