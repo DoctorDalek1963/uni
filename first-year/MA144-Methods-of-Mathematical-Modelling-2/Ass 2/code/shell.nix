9fe1d22e3eccc12a424108637dcb4e4ffbc0fb34
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-02-25 18:42:57 +0000

    Update shell.nix to export PYTHONPATH

--- first-year/MA144-Methods-of-Mathematical-Modelling-2/Ass 2/code/shell.nix
+++ first-year/MA144-Methods-of-Mathematical-Modelling-2/Ass 2/code/shell.nix
@@ -7,4 +7,8 @@
 in
   pkgs.mkShell {
     buildInputs = [python];
+
+    shellHook = ''
+      export PYTHONPATH="${python}/${python.sitePackages}"
+    '';
   }
--- first-year/MA144-Methods-of-Mathematical-Modelling-2/Ass 3/code/shell.nix
+++ first-year/MA144-Methods-of-Mathematical-Modelling-2/Ass 3/code/shell.nix
@@ -3,4 +3,8 @@
 in
   pkgs.mkShell {
     buildInputs = [python];
+
+    shellHook = ''
+      export PYTHONPATH="${python}/${python.sitePackages}"
+    '';
   }
