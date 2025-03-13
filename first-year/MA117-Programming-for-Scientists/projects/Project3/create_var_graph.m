fba21282328284fe81887555c3e0afb0f24ab14d
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-03-13 00:38:54 +0000

    Add create_var_graph.m to create VarGraph.pdf for MA117 Project 3

---
+++ first-year/MA117-Programming-for-Scientists/projects/Project3/create_var_graph.m
@@ -0,0 +1,16 @@
+load "variance.data";
+
+subplot(211);
+semilogy(variance(:,1), variance(:,2), "r");
+title("Variance of GeneralMatrix");
+xlabel("Dimension");
+ylabel("Variance");
+
+subplot(212);
+semilogy(variance(:,1), variance(:,3), "b");
+title("Variance of TriMatrix");
+xlabel("Dimension");
+ylabel("Variance");
+
+orient landscape;
+saveas(1, "VarGraph.pdf");
