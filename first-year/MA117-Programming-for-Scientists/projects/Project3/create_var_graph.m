598aae4d128367c6feafeb22ec6b3bd10819af89
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-03-17 14:49:51 +0000

    Use gnuplot for headless Octave build

--- first-year/MA117-Programming-for-Scientists/projects/Project3/create_var_graph.m
+++ first-year/MA117-Programming-for-Scientists/projects/Project3/create_var_graph.m
@@ -12,5 +12,10 @@ title("Variance of TriMatrix");
 xlabel("Dimension");
 ylabel("Variance");
 
+% NOTE: Uncomment these lines and run Octave wit FLTK or Qt to get slightly better
+% graphs for final submission. Gnuplot only labels down to 10^-15 on the y axis
+graphics_toolkit("gnuplot");
+set(1, "visible", "off");
+
 orient("landscape");
 saveas(1, "VarGraph.pdf");
