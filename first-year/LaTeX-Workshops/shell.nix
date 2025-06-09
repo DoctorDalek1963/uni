3fc3da18ecec3ff9bfd9934628beb67e89b1b538
DoctorDalek1963 <dyson.dyson@icloud.com>
2025-06-09 22:29:19 +0100

    Make shell.nix closure smaller for LaTeX-Workshops

--- first-year/LaTeX-Workshops/shell.nix
+++ first-year/LaTeX-Workshops/shell.nix
@@ -1,6 +1,30 @@
 {pkgs ? import <nixpkgs> {}}: let
-  texlive = pkgs.texlive.withPackages (p: with p; [scheme-full]);
+  texlive = pkgs.texlive.withPackages (p:
+    with p; [
+      scheme-medium
+
+      bclogo
+      esdiff
+      framed
+      gensymb
+      luapstricks
+      mdframed
+      multirow
+      needspace
+      pst-blur
+      pst-coil
+      pst-grad
+      pst-node
+      pst-tools
+      pstricks
+      tabu
+      wrapfig
+      zref
+    ]);
 in
   pkgs.mkShell {
-    buildInputs = [texlive pkgs.texmaker];
+    buildInputs = [
+      texlive
+      # pkgs.texmaker
+    ];
   }
